c28e97e726ca9c1b319a4a987738f9adb490a510
Nurasyl <nurasyl101006@icloud.com>
2026-01-27 09:47:39 +0500

    decor code

---
+++ in.py
@@ -0,0 +1,20 @@
+import  time
+
+def main(func):
+    lst  = []
+    def outer(*args):
+        start = time.time()
+        num = func(*args)
+        stop = time.time()
+        res = stop - start
+        lst.append({num: res})
+        return  lst
+    return  outer
+
+@main
+def deco(a, b):
+    res =  a + b
+    return  res
+print(deco(5 , 10))
+print(deco(50 , 100))
+
